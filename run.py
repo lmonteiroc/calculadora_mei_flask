e8b96df8927a66bbedb9f514b49948119245aeab
Luciano Monteiro <lucianomcaprara@gmail.com>
2025-11-25 13:36:28 -0300

    Ajusta run.py para produção no Render

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from app import create_app
 
+# esta variável PRECISA se chamar "app" (é o que o gunicorn usa)
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    # debug=False em produção
+    app.run(host="0.0.0.0", port=5000, debug=False)
